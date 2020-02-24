b3486b955881130781adea7a31ec957744c1e592
Sevo98 <sevo98.sk@gmail.com>
2020-02-24 23:46:43 +0700

    Completed algorithms, arrays, function tasks

--- OOP_Lab1.cpp
+++ OOP_Lab1.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include "Algorithms and Debug.h"
 #include "Arrays.h"
+#include "Functions.h"
 using namespace std;
 
 int main()
@@ -64,6 +65,57 @@ int main()
 							break;
 					}
 					
+				}
+			}
+		}
+		break;
+		//Массивы
+		case 3:
+		{
+			while (true)
+			{
+				int num = 0;
+				cout << "Задание 1" << endl;
+				cout << "Задание 2" << endl;
+				cout << "Задание 3" << endl;
+				cout << "Задание 4" << endl;
+
+				cin >> num;
+				switch (num)
+				{
+					case 1: 
+					{
+						double base;
+						int exponent;
+						cout << "Введите base: ";
+						cin >> base;
+						cout << "\nВведите exponent: ";
+						cin >> exponent;
+									   
+						double getValue;
+						getValue = GetPower(base, exponent);
+						cout << base << " ^ " << exponent << " = " << getValue << endl;
+
+					}
+						break;
+					case 2:
+					{
+						double base;
+						int exponent;
+						cout << "Введите base: ";
+						cin >> base;
+						cout << "\nВведите exponent: ";
+						cin >> exponent;
+						DemoGetPower(base, exponent);
+
+					} break;
+					case 3:
+					{
+						cout << "Введите округляемое значение" << endl;
+						int value;
+						cin >> value;
+						RoundToTens(value);
+					} break;
 				}
 			}
 		}
